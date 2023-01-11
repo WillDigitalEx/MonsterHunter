95b80c381d04c86ee714c99dab2a39a398d2df20
William Goodwin <willdigital@outlook.com>
2023-01-11 17:06:26 -0600

    Added Add to cart functionality; Cart icon numbers update, items added to cart show in cart dropdown

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,9 +1,17 @@
-import './product-card.styles.scss';
+import { useContext } from 'react';
+
+import { CartContext } from '../../contexts/cart.context';
 
 import Button from '../button/button.component';
 
+import './product-card.styles.scss';
+
 const ProductCard = ({ product }) => {
     const { name, price, imageUrl } = product;
+    const { addItemToCart } = useContext(CartContext);
+
+    const addProductTooCart = () => addItemToCart(product);
+
     return (
         <div className="product-card-container">
             <img src={imageUrl} alt={`${name}`} />
@@ -12,12 +20,13 @@ const ProductCard = ({ product }) => {
                 <span className="product-price">{price} Gold</span>
             </div>
             <Button
-                        buttonText="Add to Cart"
-                        buttonType = "default"
-                        buttonOptions= {{
-                            type: 'button',
-                        }}
-                    >
+                buttonText="Add to Cart"
+                buttonType = "default"
+                buttonOptions= {{
+                    type: 'button',
+                    onClick: addProductTooCart,
+                }}
+            >
             </Button>
         </div>
     );

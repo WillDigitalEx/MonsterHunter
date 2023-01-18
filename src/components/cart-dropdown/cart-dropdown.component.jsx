bcd757b1514ad130fa5def07693ab19910b35cf7
William Goodwin <willdigital@outlook.com>
2023-01-18 17:25:48 -0600

    Added checkout page, add or reduce number of items or remove the whole item

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { CartContext } from '../../contexts/cart.context';
 
@@ -9,6 +10,11 @@ import './cart-dropdown.styles.scss';
 
 const CartDropdown = () => {
     const { cartItems } = useContext(CartContext);
+    const navigate = useNavigate();
+
+    const goToCheckout = () => {
+        navigate('/checkout');
+    }
 
     return (
         <div className='cart-dropdown-container'>
@@ -22,6 +28,7 @@ const CartDropdown = () => {
                         buttonType = "inverted" //options: default, inverted, google
                         buttonOptions= {{
                             type: 'button',
+                            onClick: goToCheckout,
                         }}
                     >
             </Button>
